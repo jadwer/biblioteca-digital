c5d710f393cb3faa3e129ff9fe4c26ef026e03ec
RGabino Ramírez <contacto@gabinoramirez.com>
2023-04-09 10:24:08 -0600

    probando

---
+++ config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCGlEeqyUBBurp_kPeKcpdDE9bNAHGC40o",
+  authDomain: "biblioteca-digital-jwr.firebaseapp.com",
+  databaseURL: "https://biblioteca-digital-jwr-default-rtdb.firebaseio.com",
+  projectId: "biblioteca-digital-jwr",
+  storageBucket: "biblioteca-digital-jwr.appspot.com",
+  messagingSenderId: "632872861499",
+  appId: "1:632872861499:web:c61a737d8cdb495037411e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
